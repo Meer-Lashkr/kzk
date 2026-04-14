c698cf31037851b5e168f56c1551b81350459295
Meer-Lashkr <m.hammza.03@gmail.com>
2026-04-14 20:53:22 +0300

    Fix Vercel Django config

--- config/settings.py
+++ config/settings.py
@@ -102,7 +102,7 @@ LOGIN_URL = "login"
 
 EMAIL_BACKEND     = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST        = os.environ.get("EMAIL_HOST")
-EMAIL_PORT        = os.environ.get("EMAIL_PORT")
+EMAIL_PORT        = int(os.environ.get("EMAIL_PORT", 587))
 EMAIL_USE_TLS     = True
 EMAIL_HOST_USER   = os.environ.get("EMAIL_HOST_USER")  # ← replace with your Gmail address
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")     # ← replace with your Gmail App Password
